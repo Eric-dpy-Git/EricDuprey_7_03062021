55a61ddc0cc27494fe5f00e861f69f60b23dd2cf
Eric <eric.dpy@gmail.com>
2021-06-09 18:46:31 +0200

    first CRUD working with Mysql

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,9 @@ const express = require("express");
 
 //const app wich call express --> create express application
 const app = express();
+// Express v4.16.0 and higher (instead body-parser)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //Export app for server.js
 module.exports = app;
@@ -23,41 +26,101 @@ const connection = mysql.createConnection({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
 });
-
-connection.connect(function (err) {
+//--------------- for Pierre --> what the difference ?
+/* connection.connect(function (err) {
   if (err) throw err;
   console.log("Database Connected!");
+}); */
+
+connection.connect((err, connection) => {
+  if (err) throw err;
+
+  console.log(`connected as id ${connection.threadId}`);
 });
 
-//instead body parser
-app.use(express.json()); //Used to parse JSON bodies
-
-app.get("/api", (req, res) => {
-  connection.query("SELECT * FROM members", (err, rows) => {
-    if (err) throw err;
-
-    res.send(rows);
-    connection.end();
-  });
-});
-
+//create a member
 app.post("/signup", (req, res) => {
   connection.query(
     "INSERT INTO members VALUES (NULL, 'Again', 'firstnstylet', 'mail@teseeet.fr')",
     (err, rows) => {
       if (err) throw err;
-      /* res.status(201); */
-      /* res.json({ message: "Request here !" }); */
       console.log(req.body);
       res.send(rows);
-      connection.end();
     }
   );
 });
 
-app.post("/api/articles", (req, res) => {});
-
-/* app.listen(3000, () => {
-  console.log("Server is running at port 3000");
+//get all members
+app.get("/members", (req, res) => {
+  connection.query("SELECT * FROM members", (err, rows) => {
+    if (err) throw err;
+    res.send(rows);
+    /* connection.end(); */
+  });
 });
- */
+
+//create from postman
+app.post("/membersForm", (req, res) => {
+  const params = req.body;
+  connection.query("INSERT INTO members SET ?", params, (err, rows) => {
+    if (err) throw err;
+    res.send(`member ${params[0].lastname} has been added.`);
+    console.log(params);
+    console.log(params[0].lastname);
+  });
+});
+
+//get one member
+app.get("/members/:id", (req, res) => {
+  connection.query(
+    "SELECT * FROM members WHERE id = ?",
+    [req.params.id],
+    (err, rows) => {
+      if (err) throw err;
+      res.send(rows);
+    }
+  );
+});
+
+//delete one member
+app.delete("/members/:id", (req, res) => {
+  connection.query(
+    "DELETE FROM members WHERE id = ?",
+    [req.params.id],
+    (err, rows) => {
+      if (err) throw err;
+      res.send(`member with id  has been removed`);
+      /* res.send(rows); */
+    }
+  );
+});
+
+//delete all member
+app.delete("/deletteAllmembers", (req, res) => {
+  connection.query("DELETE FROM members", [req.params.id], (err, rows) => {
+    if (err) throw err;
+    /* res.send(`memberwith  ID : ${[req.params.id]} has been removed`); */
+    res.send(rows);
+  });
+});
+
+//update a member
+app.put("/modifiMember", (req, res) => {
+  const { id, lastname, firstname, email } = req.body[0];
+
+  connection.query(
+    "UPDATE members SET lastname = ? WHERE id = ?",
+    [lastname, id],
+    (err, rows) => {
+      if (err) throw err;
+      res.send(`member ${lastname} modified`);
+    }
+  );
+});
+
+/* app.use("/", (req, res, next) => {
+  const params = req.body;
+  console.log(params);
+  console.log(req.body[0].id);
+  next();
+}); */
