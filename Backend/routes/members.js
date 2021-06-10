fed092efc5f0ea8b0b78f56d03d8a1680a2ca890
Eric <eric.dpy@gmail.com>
2021-06-10 18:47:05 +0200

    Create controlleur

--- Backend/routes/members.js
+++ Backend/routes/members.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
+const apiCtrl = require("../controllers/members");
+//----------------------------------------------------working on controleur
 //import Mysql
-const mysql = require("mysql");
-
+/* const mysql = require("mysql"); */
 //create process.env to secure DB access
 //https://www.coderrocketfuel.com/article/store-mongodb-credentials-as-environment-variables-in-nodejs
-const dotenv = require("dotenv");
+/* const dotenv = require("dotenv");
 dotenv.config();
 
 const connection = mysql.createConnection({
@@ -19,87 +19,33 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-});
+}); */
 
 //create a member
-router.post("/signup", (req, res) => {
+/* router.post("/signup", (req, res) => {
   connection.query(
     "INSERT INTO members VALUES (NULL, 'Again', 'firstnstylet', 'mail@teseeet.fr')",
     (err, rows) => {
-      if (err) throw err;
-      /* console.log(req.body); */
-      res.send(rows);
+      if (err) throw err; */
+/* console.log(req.body); */
+/*  res.send(rows);
     }
   );
-});
-
-//get all members
-router.get("/members", (req, res) => {
-  connection.query("SELECT * FROM members", (err, rows) => {
-    if (err) throw err;
-    res.send(rows); //send number 200
-    /* connection.end(); */
-  });
-});
+}); */
+//-------------------------------------------working on controlleur
 
 //create from postman
-router.post("/membersForm", (req, res) => {
-  const params = req.body;
-  connection.query("INSERT INTO members SET ?", params, (err) => {
-    if (err) throw err;
-    res.send(`member ${params.lastname} has been added.`);
-    /* console.log(params);
-    console.log(params.lastname); */
-  });
-});
-
+router.post("/membersForm", apiCtrl.createFromPostman);
+//get all members
+router.get("/members", apiCtrl.getAllMemmbers);
 //get one member
-router.get("/members/:id", (req, res) => {
-  connection.query(
-    "SELECT * FROM members WHERE id = ?",
-    [req.params.id],
-    (err, rows) => {
-      if (err) throw err;
-      /* res.send(`member ${req.params.id} has been choose.`); */
-      res.send(rows);
-    }
-  );
-});
-
+router.get("/members/:id", apiCtrl.getOneMember);
 //delete one member
-router.delete("/members/:id", (req, res) => {
-  /* const test = req.body.lastname; */
-  connection.query(
-    "DELETE FROM members WHERE id = ?",
-    [req.params.id],
-    (err) => {
-      if (err) throw err;
-      /* console.log(test); */
-      res.send(`member ${req.params.id} has been removed`);
-    }
-  );
-});
-
+router.delete("/members/:id", apiCtrl.deleteOneMember);
 //delete all member
-router.delete("/deletteAllmembers", (req, res) => {
-  connection.query("DELETE FROM members", [req.params.id], (err, rows) => {
-    if (err) throw err;
-    res.send(/* `member ${req.params.id} has been removed` */ rows);
-  });
-});
-
+router.delete("/deletteAllmembers", apiCtrl.deleteAllMembers);
 //update a member
-router.put("/modifiMember", (req, res) => {
-  const { id, lastname, firstname, email } = req.body;
-  connection.query(
-    "UPDATE members SET lastname = ? WHERE id = ?",
-    [lastname, id],
-    (err, rows) => {
-      if (err) throw err;
-      res.send(/* `member ${lastname} modified` */ rows);
-    }
-  );
-});
+router.put("/updateMember/:id", apiCtrl.uptadteMember);
 
 //________________________________________________________
 
@@ -116,36 +62,12 @@ router.post("/publication", (req, res) => {
 });
 
 //create publication from postman
-router.post("/publicationForm", (req, res) => {
-  const params = req.body;
-  connection.query("INSERT INTO articles SET ?", params, (err, rows) => {
-    if (err) throw err;
-    res.send(`member ${params.title} has been added.`);
-    console.log(params);
-    console.log(params.title);
-  });
-});
+router.post("/publicationForm", apiCtrl.cretaArticle);
 
 //get all article
-router.get("/publication", (req, res) => {
-  connection.query("SELECT * FROM articles", (err, rows) => {
-    if (err) throw err;
-    res.send(rows);
-    /* connection.end(); */
-  });
-});
+router.get("/publication", apiCtrl.getAllArticles);
 
-//update a member
-router.put("/modifiArticle", (req, res) => {
-  const { id, title, id_member, texte } = req.body;
-  connection.query(
-    "UPDATE articles SET title = ?, texte = ? WHERE id = ?",
-    [title, texte, id],
-    (err, rows) => {
-      if (err) throw err;
-      res.send(`Article modified`);
-    }
-  );
-});
+//update an article
+router.put("/updateArticle/:id", apiCtrl.updateArticle);
 
 module.exports = router;
