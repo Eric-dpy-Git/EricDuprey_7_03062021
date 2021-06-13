e8eee138343fe0273002faa03df596db32e281af
Eric <eric.dpy@gmail.com>
2021-06-13 16:39:21 +0200

    Create msql Db witn sequelize and sql workbench

---
+++ Backend/migrations/20210613140057-create-message.js
@@ -0,0 +1,88 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Messages", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      attachment: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      likes: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Messages");
+  },
+};
+
+/* 'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      idUSERS: {
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      attachement: {
+        type: Sequelize.STRING
+      },
+      likes: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Messages');
+  }
+}; */
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,16 +1,38 @@
 "use strict";
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define(
+    "User",
+    {
+      email: DataTypes.STRING,
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+      bio: DataTypes.STRING,
+      isAdmin: DataTypes.BOOLEAN,
+    },
+    {
+      classMethods: {
+        associate: function (models) {
+          // associations can be defined here
+          models.User.hasMany(models.Message);
+        },
+      },
+    }
+  );
+  return User;
+};
+
+/* ("use strict");
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      models.User.hasMany(models.Message);
-    }
+  class User extends Model { */
+/**
+ * Helper method for defining associations.
+ * This method is not a part of Sequelize lifecycle.
+ * The `models/index` file will call this method automatically.
+ */
+/*   static associate(models) { */
+// define association here
+/*  }
   }
   User.init(
     {
@@ -27,3 +49,4 @@ module.exports = (sequelize, DataTypes) => {
   );
   return User;
 };
+ */
