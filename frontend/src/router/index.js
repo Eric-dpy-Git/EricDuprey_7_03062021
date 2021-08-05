97d2dc482f6a332e0917ec82e474a425d545eaee
Eric <eric.dpy@gmail.com>
2021-08-05 11:33:36 +0200

    route level code-splitting to improve load

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,37 +1,52 @@
 import Login from "../views/Auth/Login.vue";
-import Signup from "../views/Auth/Signup.vue";
-import Home from "../views/Home/Home.vue";
-import AdminHome from "../views/Admin/AdminHome.vue";
-import AdminAllUsers from "../views/Admin/AdminAllUsers.vue";
-import AdminOneUser from "../views/Admin/AdminOneUser.vue";
-import MyProfile from "../views//Profile/MyProfile.vue";
-import ModifyProfile from "../views//Profile/ModifyProfile.vue";
-import DeleteProfile from "../views//Profile/DeleteProfile.vue";
-import NewMessage from "../views//Message/NewMessage.vue";
-import ModifyMessage from "../views//Message/ModifyMessage.vue";
-import OneMessage from "../views/Message/OneMessage.vue";
-import DeleteComment from "../views//Comment/DeleteComment.vue";
-
+//route level code-splitting
 export default [
   { path: "/", component: Login },
-  { path: "/Signup", component: Signup },
+  { path: "/Signup", component: () => import("../views/Auth/Signup.vue") },
 
-  { path: "/Home", component: Home },
+  { path: "/Home", component: () => import("../views/Home/Home.vue") },
 
-  { path: "/Home/AdminHome", component: AdminHome },
-  { path: "/Home/AdminHome/AllUsers", component: AdminAllUsers },
-  { path: "/Home/AdminHome/OneUser/:id", component: AdminOneUser },
+  {
+    path: "/Home/AdminHome",
+    component: () => import("../views/Admin/AdminHome.vue"),
+  },
+  {
+    path: "/Home/AdminHome/AllUsers",
+    component: () => import("../views/Admin/AdminAllUsers.vue"),
+  },
+  {
+    path: "/Home/AdminHome/OneUser/:id",
+    component: () => import("../views/Admin/AdminOneUser.vue"),
+  },
 
-  { path: "/Home/MyProfile", component: MyProfile },
-  { path: "/Home/ModifyProfile", component: ModifyProfile },
-  { path: "/Home/DeleteProfile", component: DeleteProfile },
+  {
+    path: "/Home/MyProfile",
+    component: () => import("../views//Profile/MyProfile.vue"),
+  },
+  {
+    path: "/Home/ModifyProfile",
+    component: () => import("../views//Profile/ModifyProfile.vue"),
+  },
+  {
+    path: "/Home/DeleteProfile",
+    component: () => import("../views//Profile/DeleteProfile.vue"),
+  },
 
-  { path: "/Home/NewMessage", component: NewMessage },
-  { path: "/Home/ModifyMessage/:messageId", component: ModifyMessage },
-  { path: "/Home/OneMessage/:messageId", component: OneMessage },
+  {
+    path: "/Home/NewMessage",
+    component: () => import("../views//Message/NewMessage.vue"),
+  },
+  {
+    path: "/Home/ModifyMessage/:messageId",
+    component: () => import("../views//Message/ModifyMessage.vue"),
+  },
+  {
+    path: "/Home/OneMessage/:messageId",
+    component: () => import("../views/Message/OneMessage.vue"),
+  },
 
   {
     path: "/Home/OneMessage/:messageId/DeleteComment/:commentId",
-    component: DeleteComment,
+    component: () => import("../views//Comment/DeleteComment.vue"),
   },
 ];
